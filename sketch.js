90d9c1aca80cfbc57c4f0707f4fd9dc822f0729a
SYDNEY GRAHAM <SLG22F@FSU.EDU>
2023-09-11 11:36:27 -0400

    Rectangle and Ellipse Experiment

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER);
+  fill("navy");
+  noStroke();
 }
 
 function draw() {
-  background(125);
+  background(171, 203, 237);
+  fill("lavender");
+  rect(200, 200, 100, 100);
 }
