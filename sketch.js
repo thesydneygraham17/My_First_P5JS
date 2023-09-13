38cd0ab8ad6a5323168e224f24900c856cfc662c
SYDNEY GRAHAM <SLG22F@FSU.EDU>
2023-09-13 00:05:31 -0400

    Josef Albers

--- sketch.js
+++ sketch.js
@@ -1,12 +1,17 @@
 function setup() {
   createCanvas(400, 400);
   rectMode(CENTER);
-  fill("navy");
+  fill("blue");
   noStroke();
 }
 
 function draw() {
   background(171, 203, 237);
-  fill("lavender");
-  rect(200, 200, 100, 100);
+  fill(135, 180, 232);
+  rect(200, 230, 300, 300);
+  fill(142, 91, 186);
+  rect(200, 260, 200, 200);
+  fill(131, 52, 140);
+  rect(200, 275, 150, 150);
+
 }
